b10989ec785d462cc15f2a53378147e954db7027
Eric Yablonowitz <ericy@jana.com>
2016-08-19 22:45:41 -0400

    workaround for outdated jana puppetlabs/apt

--- manifests/repository/apt.pp
+++ manifests/repository/apt.pp
@@ -16,22 +16,19 @@ class graylog::repository::apt(
     group  => 'root',
     mode   => '0444',
     source => 'puppet:///modules/graylog/graylog-keyring.gpg',
-    notify => Exec['apt_update'],
+    #notify => Exec['apt_update'],
   }
 
   apt::source { 'graylog':
-    comment  => 'The official Graylog package repository',
-    location => $url,
-    release  => $release,
-    repos    => $version,
-    include  => {
-      'deb' => true,
-      'src' => false,
-    },
-    require  => [
+    comment     => 'The official Graylog package repository',
+    location    => $url,
+    release     => $release,
+    repos       => $version,
+    include_src => false,
+    require     => [
       File[$gpg_file],
       Package[$apt_transport_package],
     ],
-    notify   => Exec['apt_update'],
+    #notify   => Exec['apt_update'],
   }
 }
